09d455bfbd4c4b661b75977ee3dba9d156034899
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-13 14:49:56 +0100

    add wilcox rank sum test

--- t_testsim_.jl
+++ t_testsim_.jl
@@ -1,8 +1,7 @@
-using DataFrames, HypothesisTests
+using DataFrames, HypothesisTests, StatsBase
 
 # -- simulate skewed data --
 function simulate_data_skew(n, skewness_level, effect_size)
-    # Simulating two groups with different levels of skewness
     group1 = rand(Chisq(skewness_level), n)
     group2 = rand(Chisq(skewness_level), n) .+ effect_size
     
@@ -15,17 +14,33 @@ function perform_ttest(group1, group2)
     return pvalue(t_test)
 end
 
+# Function to perform Wilcoxon test
+function perform_wilcox_test(group1, group2)
+    wilcox_test = MannWhitneyUTest(group1, group2)
+    return pvalue(wilcox_test)
+end
+
 function simulation_study_skew(n, effect_size, iterations, skewness_level)
-    power_count = 0
+    power_count_ttest = 0
+    power_count_wilcox = 0
+
     for i in 1:iterations
         group1, group2 = simulate_data_skew(n, skewness_level, effect_size)
-        p_val = perform_ttest(group1, group2)
-        if p_val < 0.05
-            power_count += 1
+        p_val_ttest = perform_ttest(group1, group2)
+        p_val_wilcox = perform_wilcox_test(group1, group2)
+
+        if p_val_ttest < 0.05
+            power_count_ttest += 1
+        end
+        if p_val_wilcox < 0.05
+            power_count_wilcox += 1
         end
     end
-    power = power_count / iterations
-    return power
+
+    power_ttest = power_count_ttest / iterations
+    power_wilcox = power_count_wilcox / iterations
+
+    return power_ttest, power_wilcox
 end
 
 # Define ranges for sample sizes and skewness levels
@@ -37,7 +52,8 @@ results_df = DataFrame()
 results_df[!, :SampleSize] = repeat(sample_sizes, inner=length(skewness_levels))
 results_df[!, :SkewnessLevel] = repeat(skewness_levels, outer=length(sample_sizes))
 # results_df[!, :Power] = 0.0 # this does not work but this does:
-results_df[!, :Power] = zeros(length(sample_sizes)*length(skewness_levels))
+results_df[!, :PowerTTest] = zeros(length(sample_sizes)*length(skewness_levels))
+results_df[!, :PowerWilcox] = zeros(length(sample_sizes)*length(skewness_levels))
 
 
 effect_size = 0.6
@@ -46,14 +62,12 @@ iterations = 1000
 # Run the simulation for each combination and store the results
 for (i, n) in enumerate(sample_sizes)
     for (j, skewness_level) in enumerate(skewness_levels)
-        power = simulation_study_skew(n, effect_size, iterations, skewness_level)
-        results_df.Power[(i-1)*length(skewness_levels) + j] = power
+        power_ttest, power_wilcox = simulation_study_skew(n, effect_size, iterations, skewness_level)
+        idx = (i-1)*length(skewness_levels) + j
+        results_df.PowerTTest[idx] = power_ttest
+        results_df.PowerWilcox[idx] = power_wilcox
     end
 end
 
 # Display the results
 show(results_df, allrows=true)
-
-# Display the results in a heatmap
-using Plots
-heatmap(sample_sizes, skewness_levels, reshape(results_df.Power, length(sample_sizes), length(skewness_levels)), xlabel="Sample Size", ylabel="Skewness Level", title="Power of t-test", c=:blues, clim=(0,1))
